09bbf29532a542ea426b2497f712f1102977a31f
Jeonghoon Park <hoondb@naver.com>
2025-08-02 18:05:43 +0900

    feat: 게시판 기능 및 인증 기능 추가
    - 스프링 부트 JPA 및 JPA Auditing
    - Mustache 프론트 화면
    - 스프링 시큐리티 및 Oauth2 로그인 기능 구현
    - 소셜 로그인 (구글, 네이버) 기능 구현

---
+++ src/test/java/com/pjhn/boot/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,70 @@
+package com.pjhn.boot.springboot.web.domain.posts;
+
+import com.pjhn.boot.springboot.domain.posts.Posts;
+import com.pjhn.boot.springboot.domain.posts.PostsRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("pjhn")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2025,7,24,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>> createdDate=" + posts.getCreatedDate() +
+                ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
